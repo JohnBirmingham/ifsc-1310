58b0258f41229d19bb0f0ccd5bcf6a86922be90c
JohnBirmingham <jrbirmingham@ualr.edu>
2025-05-12 02:32:26 +0000

    added final touches

---
+++ french-horn-final/assets/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 1000,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 4000
+    });
+
+});
